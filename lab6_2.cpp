dd27a68a7c1b4655ac73c88beaaccd850bfa1b4e
shokun2349 <shokun2349@gmail.com>
2025-12-10 15:39:02 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
+double deg2rad(double degree){
+    return degree * M_PI / 180.0;
+}
+double rad2deg(double radian){
+    return radian * 180.0 / M_PI;
+}
+double findXComponent(double l1, double l2, double a1, double a2){
+    return l1 * cos(a1) + l2 * cos(a2);
+}
+double findYComponent(double l1, double l2, double a1, double a2){
+    return l1 * sin(a1) + l2 * sin(a2);
+}
+double pythagoras(double x, double y){
+    return sqrt(x*x + y*y);
+}
+void showResult(double length, double direction){
+    cout << "Resultant vector length: " << length << "\n";
+    cout << "Resultant vector direction (deg): " << direction << "\n";
+}
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
